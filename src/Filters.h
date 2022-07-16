a54b1e682461513e380d987697752f65e484bee5
TheToasterMonster <frankqiang5040@gmail.com>
2022-07-15 23:47:15 -0500

    added prewitt operator

--- src/Filters.h
+++ src/Filters.h
@@ -2,17 +2,29 @@
 #include "Image.h"
 
 #define SOBEL_OPERATOR_2D Filter::SobelGx, Filter::SobelGy
+#define PREWITT_OPERATOR_2D Filter::PrewittGx, Filter::PrewittGy
 
 namespace Filter
 {
 	const Operator SobelGx = {
+		{ -1,  0,  1 },
+		{ -2,  0,  2 },
+		{ -1,  0,  1 },
+	};
+	const Operator SobelGy = {
 		{ -1, -2, -1 },
 		{  0,  0,  0 },
 		{  1,  2,  1 },
 	};
-	const Operator SobelGy = {
+
+	const Operator PrewittGx = {
 		{ -1,  0,  1 },
-		{ -2,  0,  2 },
 		{ -1,  0,  1 },
+		{ -1,  0,  1 },
+	};
+	const Operator PrewittGy = {
+		{ -1, -1, -1 },
+		{  0,  0,  0 },
+		{  1,  1,  1 },
 	};
 }
